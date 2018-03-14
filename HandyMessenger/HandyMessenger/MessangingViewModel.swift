367023ffab3d03330c322846d5db8f76a88f44f3
aturkin <andrewturk30@gmail.com>
2018-03-14 22:44:51 +0100

    Added base structure

---
+++ HandyMessenger/HandyMessenger/MessangingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MessangingViewModel.swift
+//  HandyMessenger
+//
+//  Created by Andrew Turkin on 3/14/18.
+//  Copyright © 2018 Andrew Turkin. All rights reserved.
+//
+
+import Foundation
+
+protocol MessangingViewModel {
+    
+}
